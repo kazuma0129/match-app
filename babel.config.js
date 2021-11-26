05536458d1b7da2fe0e0ca0610f96bec3be8fe29
KazumaOhashi <tikuwaaaaaaaa@gmail.com>
2021-11-26 20:39:10 +0900

    Move: ./src

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,16 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: [
+      'react-native-reanimated/plugin',
+      [
+        'module-resolver',
+        {
+          root: ['./src/'],
+          extensions: ['.ts', '.tsx', '.mjs', '.js', '.jsx'],
+        },
+      ],
+    ],
     presets: ['babel-preset-expo'],
   };
 };
